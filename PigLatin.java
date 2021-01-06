bb3b46d73d3020dce7c684b76316e790b386bd6c
i has candys <ichen10@stuy.edu>
2021-01-06 00:09:58 -0500

    added pigLatin II method + tested

--- PigLatin.java
+++ PigLatin.java
@@ -12,19 +12,63 @@ public class PigLatin {
 			}
 		}
 		if (firstLetterIsVowel) {
-			newWord += oldWord + "hay";
+			newWord = oldWord + "hay";
 		} else {
-			newWord += oldWord.substring(1) + firstLetter + "ay";
+			newWord = oldWord.substring(1) + firstLetter + "ay";
 		}
 		return newWord;
 	}
+	public static String pigLatin(String s) {
+		if (s.length() == 1) {
+			return pigLatinSimple(s);
+		}
+		String oldWord = s.toLowerCase();
+		String newWord = "";
+		String[] vowels = {"a", "e", "i", "o", "u"};
+		String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+		String firstTwoLetters = oldWord.substring(0, 2);
+		boolean firstLetterIsVowel = false;
+		boolean firstTwoLettersIsDigraph = false;
+		for (String vowel: vowels) {
+			if (firstTwoLetters.substring(0, 1).equals(vowel)) {
+				firstLetterIsVowel = true;
+			}
+		}
+		for (String digraph: digraphs) {
+			if (firstTwoLetters.equals(digraph)) {
+				firstTwoLettersIsDigraph = true;
+			}
+		}
+
+		if (firstLetterIsVowel) {
+			newWord = oldWord + "hay";
+		}
+		else if (firstTwoLettersIsDigraph) {
+			newWord = oldWord.substring(2) + firstTwoLetters + "ay";
+		}
+		else {
+			newWord = oldWord.substring(1) + oldWord.substring(0, 1) + "ay";
+		}
+
+		return newWord;
+	}
 	public static void main ( String[] args ) {
+		// Scanner n = new Scanner( System.in );
+		// while (n.hasNextLine()) {
+		// 	Scanner line = new Scanner( n.nextLine() );
+		// 	while (line.hasNext()) {
+		// 		String word = line.next();
+		// 		System.out.print(pigLatinSimple(word));
+		// 		System.out.print(" ");
+		// 	}
+		// 	System.out.println();
+		// }
 		Scanner n = new Scanner( System.in );
 		while (n.hasNextLine()) {
 			Scanner line = new Scanner( n.nextLine() );
 			while (line.hasNext()) {
 				String word = line.next();
-				System.out.print(pigLatinSimple(word));
+				System.out.print(pigLatin(word));
 				System.out.print(" ");
 			}
 			System.out.println();
